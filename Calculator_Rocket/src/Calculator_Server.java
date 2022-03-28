5124ef7e0e6f4c810a6f99da3a48aae87aa2a686
DuongTuHieu <Tuhieu9a1.vip@gmail.com>
2022-03-28 15:36:43 +0700

    go to

---
+++ Calculator_Rocket/src/Calculator_Server.java
@@ -0,0 +1,68 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Calculator_Server {
+	 public static void main(String []args) throws IOException
+	    {
+	        String so1,so2,pheptoan,kqstr,traloi;
+	        double ketqua = 0;
+	        // tao server socket
+	        ServerSocket server = new ServerSocket(1234);
+	        System.out.println("Server is now already");
+	        //tao 1 socket do ket noi tu client toi server
+	        Socket connectionSocket = server.accept();
+	        //tao luong nhan du lieu tu client
+	        DataInputStream inFromClient = new DataInputStream(connectionSocket.getInputStream());
+	        // tao luong gui du lieu toi client
+	        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+	        do {
+	            outToClient.writeBytes("Mời nhập a: " + '\n');
+	            // truyen du lieu tu client vao 2 bien so1 va so2
+	            so1 = inFromClient.readLine();
+	            outToClient.writeBytes("Mời nhập b: " + '\n');
+	            so2 = inFromClient.readLine();
+	            outToClient.writeBytes("Mời nhập phép toán: " + '\n');
+	            pheptoan = inFromClient.readLine();
+
+	            //ep so1 va so2 tu kieu String sang kieu Integer
+	            int a = Integer.parseInt(so1);
+	            int b = Integer.parseInt(so2);
+	            switch (pheptoan) {
+	                case "+":
+	                    ketqua = a + b;
+	                    break;
+	                case "-":
+	                    ketqua = a - b;
+	                    break;
+	                case "*":
+	                    ketqua = a * b;
+	                    break;
+	                case "/":
+	                    if (b == 0) {
+	                        kqstr = "ERROR b=0";
+	                        break;
+	                    }
+	                    ketqua = a * 1.0f / b;
+	                    break;
+	            }
+	            //ep ketqua 2 so sang kieu String
+	            kqstr = String.valueOf(ketqua);
+	            //gui kqstr ve client
+	            outToClient.writeBytes(kqstr + '\n');
+
+	            outToClient.writeBytes("Bạn có muốn làm tiếp hay không(Co/ko): " + '\n');
+	            traloi = inFromClient.readLine();
+	        }while(traloi.equalsIgnoreCase("Co"));
+
+	        //dong luong nhan du lieu tu client
+	        inFromClient.close();
+	        //dong luong gui du lieu ve client
+	        outToClient.close();
+	        //dong server socket
+	        server.close();
+	    }
+}
